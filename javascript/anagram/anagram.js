ba2b0e285d2d423927cff9d061b7b9ac847962ea
loïc moriamé <loic@moriame.fr>
2013-08-20 19:29:10 +0200

    Some refactoring to have a cleaner code :
    * use array.forEach() to parse Words
    * add some String.prototype to compare to words together to know if they are anagram
    * add some Array.prototype to have quicker array comparison & manipulation

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -4,38 +4,96 @@ function Anagram(word) {
   this.word = word;
 }
 
-
-Anagram.prototype.match = function(words) {
-  var matches = [];
-
-  for(var i=0; i<words.length; i++) {
-    var currentWord = words[i];
-    
-    if (currentWord.isAnagrameOf(this.word)) {
-      matches.push(currentWord);
-    };
+function searchAnagram (value, index, array) {
+  if(!value.isAnagrameOf(this.word)) {
+    array[index] = null;
   }
-
-  return matches;
 };
 
-String.prototype.isAnagrameOf = function(word) {
-  //console.log(word, this);
+Anagram.prototype.match = function(words) {
+  words.forEach(searchAnagram, this);
+  words.cleanEmptyValue();
+  
+  return words;
+};
 
-  if(word.length !== this.length) {
+
+/**
+ * Compare to words together to know if they are anagram each of the other
+ *
+ * @param {String} value The `value` to compare
+ * @returns {Boolean} Returns `true` if the `value` is an anagram, else `false`.
+ */
+String.prototype.isAnagrameOf = function(value) {
+  var wordLetters     = value.toUpperCase().split('').sort(),
+      anagramLetters  = this.toUpperCase().split('').sort();
+
+  return wordLetters.isEqualTo(anagramLetters);
+};
+
+/**
+ * Compare to arrays together to know if they are equal
+ * Can only compare two one-dimensional array
+ *
+ * @param {Array} array The `array` to compare
+ * @returns {Boolean} Returns `true` if the `array` is equal, else `false`.
+ */
+Array.prototype.isEqualTo = function(array) {
+  if(!array) {
     return false;
   }
-
-  var wordLetters     = word.split('').sort(),
-      anagramLetters  = this.split('').sort();
-
-  for(var i=0; i<wordLetters.length; i++) {
-    if(wordLetters[i] !== anagramLetters[i]) {
+ 
+  if(!(array instanceof Array)) {
+    return false;
+  }
+ 
+  if(this.length != array.length) {
+    return false;
+  }
+ 
+  var i = this.length;
+  while (i--) {
+    if (this[i] !== array[i]) {
       return false;
     }
   }
-
+ 
   return true;
 };
+
+/**
+ * Remove the first or all value from an array
+ *
+ * @param {String} value The `value` to search
+ * @param {Boolean} global Search all values (default : false)
+ * @returns {Array} Returns the array without the search `value`.
+ */
+Array.prototype.unset = function(value, global){
+    var index   = this.indexOf(value),
+        global  = global || false;
+ 
+    while(index > -1) {
+      this.splice(index, 1);
+      index = global ? this.indexOf(value) : -1;
+    }
+}
+
+/**
+ * Remove all falsy value from an array
+ * Values `false`, `null`, `0`, `""`, `undefined`, and `NaN` are all falsey.
+ *
+ * @param {String} value The `value` to search
+ * @param {Boolean} global Search all values (default : false)
+ * @returns {Array} Returns the array without the search `value`.
+ */
+Array.prototype.cleanEmptyValue = function(){
+    this.unset(false, true);
+    this.unset(null, true);
+    this.unset(0, true);    
+    this.unset('', true);
+    this.unset(undefined, true);
+    this.unset(NaN, true);
+}
+
 
 module.exports = Anagram;
