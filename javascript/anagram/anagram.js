5ea6b2b90faf7c79b6968b1bf1b5dc10cff75093
Loïc Moriamé <loic@moriame.fr>
2013-08-26 13:55:43 +0200

    Remove prototype of Array to remove emptu value : replace by Array#filter

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -7,9 +7,7 @@
 
   Anagram.prototype.match = function(words) {
     words.forEach(this.checkWords, this.word);
-    words.clearEmptyValue();
-    
-    return words;
+    return words.filter(this.filterEmptyValue);
   };
 
   Anagram.prototype.checkWords = function(word, index, array) {
@@ -18,22 +16,16 @@
     }
   }
 
+  Anagram.prototype.filterEmptyValue = function(word) {
+    return word !== null;
+  }
+
   String.prototype.isAnagrameOf = function(word) {
     var word1 = this.toUpperCase().split('').sort().join(''),
         word2 = word.toUpperCase().split('').sort().join('');
 
     return word1 === word2;
   };
-
-  Array.prototype.clearEmptyValue = function() {
-    var value   = null,
-        index   = this.indexOf(value);
-
-    while(index > -1) {
-      this.splice(index, 1);
-      index = this.indexOf(value);
-    }
-  }
 
   module.exports = Anagram;
 
